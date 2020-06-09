13915ffc48e21bda4b2ad8492c50b499b1e177de
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-09 03:38:04 +0000

    Deploy wxsms/vue-md-loader to github.com/wxsms/vue-md-loader.git:gh-pages

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <markdown />
+  </div>
+</template>
+
+<script>
+import Markdown from './markdown.md'
+import 'highlight.js/styles/github-gist.css'
+
+export default {
+  components: { Markdown }
+}
+</script>
+
+<style>
+  .live-wrapper {
+    background: #eee;
+    padding: 10px;
+  }
+</style>
